89acc8ca86f5ad7258df7f93dfd21e208ae310c4
Bhanu Teja <18211a04h0@bvrit.ac.in>
2021-11-07 23:35:15 +0530

    Add 10382 11292 12390

--- UVa online judge/UVa10382.cpp
+++ UVa online judge/UVa10382.cpp
@@ -1,5 +1,3 @@
-// Kattis - grass / UVa 10382 - Watering Grass
-
 #include <bits/stdc++.h>
 using namespace std;
  
@@ -18,37 +16,36 @@ bool cmp(sp a, sp b) {
 }
 
 int main() {
-  // freopen("in.txt", "r", stdin);
   int n, l, w;
   while (scanf("%d %d %d", &n, &l, &w) != EOF) {
     for (int i = 0; i < n; ++i) {
       scanf("%d %d", &sprinkler[i].x, &sprinkler[i].r);
       if (2*sprinkler[i].r >= w) {
         double d_x = sqrt((double)sprinkler[i].r*sprinkler[i].r - (w/2.0)*(w/2.0));
-        sprinkler[i].x_l = sprinkler[i].x-d_x; // sort based on smaller x_l and then larger x_r
+        sprinkler[i].x_l = sprinkler[i].x-d_x;
         sprinkler[i].x_r = sprinkler[i].x+d_x;
       }
       else
-        sprinkler[i].x_l = sprinkler[i].x_r = sprinkler[i].x; // to make this unselected...
+        sprinkler[i].x_l = sprinkler[i].x_r = sprinkler[i].x;
     }
  
-    sort(sprinkler, sprinkler+n, cmp);         // sort the sprinklers
+    sort(sprinkler, sprinkler+n, cmp);
     bool possible = true;
     double covered = 0.0;
     int ans = 0;
     for (int i = 0; (i < n) && possible; ++i) {
-      if (covered > l) break;                  // done
-      if (sprinkler[i].x_r < covered+EPS) continue; // inside prev interval
-      if (sprinkler[i].x_l < covered+EPS) {    // can cover
+      if (covered > l) break;
+      if (sprinkler[i].x_r < covered+EPS) continue;
+      if (sprinkler[i].x_l < covered+EPS) {
         double max_r = -1.0;
         int max_id;
         for (int j = i; (j < n) && (sprinkler[j].x_l < covered+EPS); ++j)
-          if (sprinkler[j].x_r > max_r) {      // go to right to find
-            max_r = sprinkler[j].x_r;          // interval with
-            max_id = j;                        // the largest coverage
+          if (sprinkler[j].x_r > max_r) {
+            max_r = sprinkler[j].x_r;
+            max_id = j;
           }
         ++ans;
-        covered = max_r;                       // jump here
+        covered = max_r;
         i = max_id;
       }
       else
